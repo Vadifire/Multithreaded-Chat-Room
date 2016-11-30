738b3373ad6faae60ffe5ef692dc1c64be548555
Ahsan Khan <ajkhan96@gmail.com>
2016-11-30 03:54:11 -0600

    Majorly restructured code to separate GUI and socket components

--- src/assignment7/Client.java
+++ src/assignment7/Client.java
@@ -1,14 +1,20 @@
 package assignment7;
 
-import java.io.*; 
-import java.net.*; 
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map.Entry;
+import java.util.Queue;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets; 
-import javafx.geometry.Pos; 
-import javafx.scene.Scene; 
-import javafx.scene.control.Label; 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.TextArea;
@@ -21,64 +27,71 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent; 
+import javafx.stage.WindowEvent;
 
-
-public class Client extends Application{ 
-	// IO streams 
-	DataOutputStream toServer = null; 
+public class Client extends Application {
+	// IO streams
+	DataOutputStream toServer = null;
 	DataInputStream fromServer = null;
-	
+
+	HashMap<Integer, ChatWindow> chats;
+
 	private String username;
-	TextArea messageLog = new TextArea();	
+	TextArea messageLog = new TextArea();
 
+	@Override
+	public void start(Stage primaryStage) {
+		chats = new HashMap<Integer, ChatWindow>();
 
-	@Override // Override the start method in the Application class 
-	public void start(Stage primaryStage) { 
+		try {
+			// Create a socket to connect to the server
+			@SuppressWarnings("resource")
+			Socket socket = new Socket("localhost", 8000);
 
-		
+			// Create an input stream to receive data from the server
+			fromServer = new DataInputStream(socket.getInputStream());
+
+			// Create an output stream to send data to the server
+			toServer = new DataOutputStream(socket.getOutputStream());
+		} catch (IOException ex) {
+			// ta.appendText(ex.toString() + '\n');
+		}
+
 		TextField login = new TextField("Enter Username:");
-		HBox hbox = new HBox();
-		Scene chatScene = new Scene(hbox, 800,600);
-		
-		VBox vbox1 = new VBox();
-		ScrollPane scrollPane = new ScrollPane(messageLog);
-		scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
-		vbox1.getChildren().add(scrollPane);
-		TextField messageInput = new TextField();
-		vbox1.getChildren().add(messageInput);
-		
-		hbox.getChildren().add(vbox1);
-		
-		Scene loginScene = new Scene(login , 200, 40);
+		/*
+		 * HBox hbox = new HBox(); Scene chatScene = new Scene(hbox, 800,600);
+		 * 
+		 * VBox vbox1 = new VBox(); ScrollPane scrollPane = new
+		 * ScrollPane(messageLog);
+		 * scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
+		 * vbox1.getChildren().add(scrollPane); TextField messageInput = new
+		 * TextField(); vbox1.getChildren().add(messageInput);
+		 * 
+		 * hbox.getChildren().add(vbox1);
+		 */
+		Scene loginScene = new Scene(login, 200, 40);
 		primaryStage.setScene(loginScene);
 		primaryStage.setX(10);
 		primaryStage.setY(10);
-		primaryStage.setTitle("Login:");
+		primaryStage.setTitle("Login");
 		primaryStage.setResizable(false);
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-		    @Override public void handle(WindowEvent t) {
-		    	System.exit(0);
-		    }
-		});
-		primaryStage.show();
-		
-		messageInput.setOnKeyPressed(new EventHandler<KeyEvent>() {
 			@Override
-			public void handle(KeyEvent event) {
-				if (event.getCode() == KeyCode.ENTER){
-					String message = messageInput.getText();
-					try {
-						toServer.writeUTF(username+": "+message);
-						messageInput.setText("");
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
+			public void handle(WindowEvent t) {
+				System.exit(0);
 			}
 		});
-		
+		primaryStage.show();
+
+		/*
+		 * messageInput.setOnKeyPressed(new EventHandler<KeyEvent>() {
+		 * 
+		 * @Override public void handle(KeyEvent event) { if (event.getCode() ==
+		 * KeyCode.ENTER){ String message = messageInput.getText(); try {
+		 * toServer.writeUTF(username+": "+message); messageInput.setText(""); }
+		 * catch (IOException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); } } } });
+		 */
 		login.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
@@ -88,73 +101,169 @@ public class Client extends Application{
 				}
 			}
 		});
-		
+
 		login.setOnKeyPressed(new EventHandler<KeyEvent>() {
 			@Override
 			public void handle(KeyEvent event) {
-				if (event.getCode() == KeyCode.ENTER){
+				if (event.getCode() == KeyCode.ENTER) {
 					username = login.getText();
-					if (username.length() > 0){
-						try {
-							primaryStage.setTitle("Global Chatroom:");
-							toServer.writeUTF(username);
-							ReadThread readThread = new ReadThread(fromServer);
-							readThread.start();
-							toServer.writeUTF(username+" has joined the room.");
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						primaryStage.setScene(chatScene);
+					if (username.length() > 0) {
+						login(username);
+						primaryStage.hide();
 					}
 				}
 			}
 		});
-		
-		try { 
-			// Create a socket to connect to the server 
-			@SuppressWarnings("resource")
-			Socket socket = new Socket("localhost", 8000); 
-			// Socket socket = new Socket("130.254.204.36", 8000); 
-			// Socket socket = new Socket("drake.Armstrong.edu", 8000); 
+	}
 
-			// Create an input stream to receive data from the server 
-			fromServer = new DataInputStream(socket.getInputStream()); 
+	private void login(String username) {
+		try {
+			toServer.writeUTF(username);
+			System.out.println("Wrote " + username);
+			ChatWindow globalChat = new ChatWindow("Global");
 
-			// Create an output stream to send data to the server 
-			toServer = new DataOutputStream(socket.getOutputStream()); 
-		} 
-		catch (IOException ex) { 
-			//ta.appendText(ex.toString() + '\n');
-		}	
+			chats.put(fromServer.readInt(), globalChat);
+
+			toServer.writeUTF(username + " has joined the room.");
+
+			new ReadThread(fromServer, chats).start();
+			new WriteThread(toServer, chats).start();
+			new GuiHandler(globalChat).start();
+		} catch (IOException e) {
+		}
 
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	class ReadThread extends Thread{
-		
-		DataInputStream fromServer;
-		
-		public ReadThread(DataInputStream fromServer){
-			this.fromServer = fromServer;
+
+	class WriteThread extends Thread {
+		DataOutputStream toServer;
+		HashMap<Integer, ChatWindow> chats;
+
+		public WriteThread(DataOutputStream toServer, HashMap<Integer, ChatWindow> chats) {
+			this.toServer = toServer;
+			this.chats = chats;
 		}
-		
-		public void run(){
-			while (true){
-				String message;
+
+		public void run() {
+			boolean done = false;
+			while (!done) {
+				for (Entry<Integer, ChatWindow> m : chats.entrySet()) {
+					while (!m.getValue().outbox.isEmpty()) {
+						try {
+							String toWrite = String.format("%04d", m.getKey()) + m.getValue().outbox.poll();
+							toServer.writeUTF(toWrite);
+						} catch (Exception e) {
+							done = true;
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+	}
+
+	class ReadThread extends Thread {
+
+		DataInputStream fromServer;
+		HashMap<Integer, ChatWindow> chats;
+
+		public ReadThread(DataInputStream fromServer, HashMap<Integer, ChatWindow> chats) {
+			this.fromServer = fromServer;
+			this.chats = chats;
+		}
+
+		public void run() {
+			String message;
+			boolean done = false;
+			while (!done) {
 				try {
 					message = fromServer.readUTF();
-					messageLog.appendText(message+"\n");
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
+					int chatID = Integer.parseInt(message.substring(0, 3));
+					chats.get(chatID).inbox.add(message.substring(4));
+				} catch (Exception e) {
+					done = true;
 					e.printStackTrace();
 				}
 			}
 		}
 	}
 
-	
+	class GuiHandler extends Thread {
+		ChatWindow chat;
+
+		public GuiHandler(ChatWindow chat) {
+			this.chat = chat;
+		}
+
+		public void run() {
+			while (true) {
+				while (!chat.inbox.isEmpty()) {
+					chat.addText(chat.inbox.poll());
+				}
+			}
+		}
+	}
+}
+
+class ChatWindow {
+	private Stage window;
+	private Scene scene;
+	private HBox content;
+	private TextArea ta;
+	Queue<String> inbox;
+	Queue<String> outbox;
+	Label l;
+
+	public void addText(String message) {
+		ta.appendText(message + "\n");
+	}
+
+	public ChatWindow(String name) {
+		l = new Label("LABEL");
+		inbox = new LinkedList<String>();
+		outbox = new LinkedList<String>();
+		ta = new TextArea();
+		content = new HBox();
+		VBox messageArea = new VBox();
+
+		ta.setEditable(false);
+		ScrollPane scrollPane = new ScrollPane(ta);
+		scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
+		messageArea.getChildren().add(scrollPane);
+		TextField messageInput = new TextField();
+		messageArea.getChildren().add(messageInput);
+		content.getChildren().add(messageArea);
+		content.getChildren().add(l);
+
+		scene = new Scene(content, 800, 400);
+		window = new Stage();
+		window.setScene(scene);
+		window.setTitle(name);
+		window.setX(10);
+		window.setY(10);
+		window.show();
+
+		window.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent arg0) {
+				window.hide();
+			}
+
+		});
+
+		messageInput.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode() == KeyCode.ENTER) {
+					if (!messageInput.getText().equals("")) {
+						outbox.add(messageInput.getText());
+					}
+					messageInput.setText("");
+				}
+			}
+		});
+	}
 }
--- src/assignment7/MultiThreadServer.java
+++ src/assignment7/MultiThreadServer.java
@@ -3,118 +3,125 @@ package assignment7;
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
+public class MultiThreadServer extends Application { // Text area for displaying
+														// contents
+	private TextArea ta = new TextArea();
 
-public class MultiThreadServer extends Application 
-{ // Text area for displaying contents 
-	private TextArea ta = new TextArea(); 
+	// private static ArrayList<ChatRoom> chatRooms = new ArrayList<ChatRoom>();
+	// private static ArrayList<User> users = new ArrayList<User>();
 
-	private static ArrayList<ChatRoom> chatRooms = new ArrayList<ChatRoom>();
-	private static ArrayList<User> users = new ArrayList<User>();
+	private static HashMap<Integer, ChatRoom> chatRooms = new HashMap<Integer, ChatRoom>();
+	private static HashMap<String, User> users = new HashMap<String, User>();
+	private static ChatRoom globalChat = new ChatRoom();
+	// Number a client
+	private int clientNo = 0;
 
-	// Number a client 
-	private int clientNo = 0; 
+	@Override // Override the start method in the Application class
+	public void start(Stage primaryStage) {
+		// Create a scene and place it in the stage
+		ta.setEditable(false);
+		Scene scene = new Scene(new ScrollPane(ta), 450, 200);
+		primaryStage.setTitle("MultiThreadServer"); // Set the stage title
+		primaryStage.setScene(scene); // Place the scene in the stage
+		primaryStage.show(); // Display the stage
 
-	@Override // Override the start method in the Application class 
-	public void start(Stage primaryStage) { 
-		// Create a scene and place it in the stage 
-		Scene scene = new Scene(new ScrollPane(ta), 450, 200); 
-		primaryStage.setTitle("MultiThreadServer"); // Set the stage title 
-		primaryStage.setScene(scene); // Place the scene in the stage 
-		primaryStage.show(); // Display the stage 
+		new Thread(() -> {
+			try { // Create a server socket
+				ServerSocket serverSocket = new ServerSocket(8000);
+				ta.appendText("MultiThreadServer started at " + new Date() + '\n');
 
-		new Thread( () -> { 
-			try {  // Create a server socket 
-				ServerSocket serverSocket = new ServerSocket(8000); 
-				ta.appendText("MultiThreadServer started at " 
-						+ new Date() + '\n'); 
+				while (true) {
+					// Listen for a new connection request
+					Socket socket = serverSocket.accept();
 
+					// Increment clientNo
+					clientNo++;
 
-				while (true) { 
-					// Listen for a new connection request 
-					Socket socket = serverSocket.accept(); 
+					Platform.runLater(() -> {
+						// Display the client number
+						ta.appendText("Starting thread for client " + clientNo + " at " + new Date() + '\n');
 
-					// Increment clientNo 
-					clientNo++; 
-
-					Platform.runLater( () -> { 
-						// Display the client number 
-						ta.appendText("Starting thread for client " + clientNo +
-								" at " + new Date() + '\n'); 
-
-						// Find the client's host name, and IP address 
+						// Find the client's host name, and IP address
 						InetAddress inetAddress = socket.getInetAddress();
-						ta.appendText("Client " + clientNo + "'s host name is "
-								+ inetAddress.getHostName() + "\n");
-						ta.appendText("Client " + clientNo + "'s IP Address is " 
-								+ inetAddress.getHostAddress() + "\n");	}); 
-
+						ta.appendText("Client " + clientNo + "'s host name is " + inetAddress.getHostName() + "\n");
+						ta.appendText("Client " + clientNo + "'s IP Address is " + inetAddress.getHostAddress() + "\n");
+					});
 
 					// Create and start a new thread for the connection
 					new Thread(new HandleAClient(socket)).start();
-				} 
-			} 
-			catch(IOException ex) { 
+				}
+			} catch (IOException ex) {
 				System.err.println(ex);
 			}
 		}).start();
 	}
 
-
 	// Define the thread class for handling
 	class HandleAClient implements Runnable, Observer {
-		
+
 		private User user;
 		private Socket socket; // A connected socket
 		DataInputStream inputFromClient;
 		DataOutputStream outputToClient;
-		
-		/** Construct a thread */ 
-		public HandleAClient(Socket socket) { 
-			
+
+		/** Construct a thread */
+		public HandleAClient(Socket socket) {
+
 			this.socket = socket;
 		}
+
 		/** Run a thread */
-		public void run() { 
+		public void run() {
 			try {
 				// Create data input and output streams
-				inputFromClient = new DataInputStream( socket.getInputStream());
-				outputToClient = new DataOutputStream( socket.getOutputStream());
-				
+				inputFromClient = new DataInputStream(socket.getInputStream());
+				outputToClient = new DataOutputStream(socket.getOutputStream());
+
 				String username = inputFromClient.readUTF();
 				user = new User(username);
-				ChatRoom globalChat = chatRooms.get(0);
+				users.put(username, user);
 				globalChat.addObserver(this);
 				user.joinRoom(globalChat);
-				
+				outputToClient.writeInt(globalChat.getId());
+
+				globalChat.postMessage(inputFromClient.readUTF());
 				// Continuously serve the client
-				while (true) { 
+				while (true) {
 					String messageReceived = inputFromClient.readUTF();
-					globalChat.postMessage(messageReceived);
+					if (messageReceived.equals("FRIENDS")) {
+						
+					} else {
+						globalChat.postMessage(String.format("(%tT) ", Calendar.getInstance()) + user.getName() + ": "
+								+ messageReceived.substring(4));
+					}
 				}
-			} catch(IOException e) {
-				chatRooms.get(0).postMessage(user.getName()+" has left the room.");
+			} catch (IOException e) {
+				chatRooms.get(0).postMessage(user.getName() + " has left the room.");
 				e.printStackTrace();
 			}
 		}
-		
-		
+
 		@Override
 		public void update(Observable arg0, Object arg1) {
 			int chatId = ((ChatRoom) arg0).getId();
 			String newMessage = (String) arg1;
 			try {
-				outputToClient.writeUTF(newMessage);
+				outputToClient.writeUTF(String.format("%04d", chatId) + newMessage);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -122,10 +129,10 @@ public class MultiThreadServer extends Application
 		}
 
 	}
-	
-	
+
 	public static void main(String[] args) {
-		chatRooms.add(new ChatRoom()); //Global chat room (id 0)
+		chatRooms.putIfAbsent(globalChat.getId(), globalChat); // Global chat
+																// room (id 0)
 		launch(args);
 	}
 }
